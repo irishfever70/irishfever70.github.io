66a3760a07c7587cf01d36090a2a955a3d2c2cce
chrnin <chrnin@users.noreply.github.com>
2021-11-11 10:35:58 +0000

    deploy: 79299e192c75f069bc10a828a0bf7050d23fb1c7

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -3,4 +3,9 @@ var store = [{
         "excerpt":"Hello this is Irish Fever talking ! ","categories": ["Layout"],
         "tags": [],
         "url": "/layout/welcome-to-irishfever/"
+      },{
+        "title": "Toto n'a pas supporté son troisième album Irish Fever!",
+        "excerpt":"So long and thanks for all the fish ! ","categories": ["toto_est_content"],
+        "tags": [],
+        "url": "/toto_est_content/good-bye-irishfever/"
       }]
