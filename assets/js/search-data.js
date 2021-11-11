f79460b23037bed8229bed64b608c26829f0fae2
fabiteby <fabiteby@users.noreply.github.com>
2021-11-11 16:24:52 +0000

    deploy: da88db99ad214e804bb1727150dedd06446ad1e9

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -5,7 +5,7 @@ var store = [{
         "url": "/layout/welcome-to-irishfever/"
       },{
         "title": "Toto n'a pas supporté son troisième album Irish Fever!",
-        "excerpt":"So long and thanks for all the fish !I’m not drunk at all ! Du coup, on peut se promener dans les fichiers et voir ce qu’on fait !     ","categories": ["toto_est_content"],
+        "excerpt":"So long and thanks for all the fish !I’m not drunk at all ! Du coup, on peut se promener dans les fichiers et voir ce qu’on fait ! Ok doc  :)turlututu chapeau pointu     ","categories": ["toto_est_content"],
         "tags": [],
         "url": "/toto_est_content/good-bye-irishfever/"
       }]
