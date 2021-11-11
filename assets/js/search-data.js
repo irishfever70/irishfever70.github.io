9e87850cf639fc9a44a566611d46e01a04751ddd
chrnin <chrnin@users.noreply.github.com>
2021-11-11 17:37:58 +0000

    deploy: 26c94518e84d516c4d2cfc12ba770264cb76e6de

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -4,6 +4,11 @@ var store = [{
         "tags": [],
         "url": "/layout/welcome-to-irishfever/"
       },{
+        "title": "20 Novembre Pusey",
+        "excerpt":"","categories": [],
+        "tags": [],
+        "url": "/20-novembre-pusey/"
+      },{
         "title": "Toto n'a pas supporté son troisième album Irish Fever!",
         "excerpt":"So long and thanks for all the fish !I’m not drunk at all ! Du coup, on peut se promener dans les fichiers et voir ce qu’on fait ! Ok doc  :)turlututu chapeau pointu     ","categories": ["toto_est_content"],
         "tags": [],
