274f6e78678bf010a74a4ccf89a8d36087b91aa4
chrnin <chrnin@users.noreply.github.com>
2021-11-12 06:19:42 +0000

    deploy: 458e79180529e3954f95c63a2e9b59d03fe00068

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -2,10 +2,10 @@ var store = [{
         "title": "Venez nous écouter le 20 novembre 2021 à Pusey!",
         "excerpt":"Venez nous retrouver à la fête du chapeau de paille à Pusey le 20 novembre 2021 ! Infos pratiques:   blabla  carte google  etc.","categories": ["concert"],
         "tags": [],
-        "url": "/concert/20-novembre-pusey/"
+        "url": "https://www.irishfever.fr//concert/20-novembre-pusey/"
       },{
         "title": "Venez nous écouter le 5 décembre quelque part!",
         "excerpt":"Venez nous retrouver au festival de la limonade ! Infos pratiques:   de 13 à 14h avec Jean-Castex en guest star !  pour la fin de soirée pensez aux masques et au gelAfficher une carte plus grande ","categories": ["concert"],
         "tags": [],
-        "url": "/concert/5-d%C3%A9cembre-quelque-part/"
+        "url": "https://www.irishfever.fr//concert/5-d%C3%A9cembre-quelque-part/"
       }]
