da15ca8b7f7a1db0ba9874b8d5c33ea84d3c1978
fabiteby <fabiteby@users.noreply.github.com>
2021-11-13 15:22:34 +0000

    deploy: b8d89bcf4ee312686352639f5914c83fe9d5186c

--- assets/js/search-data.js
+++ assets/js/search-data.js
@@ -1,16 +1,16 @@
 var store = [{
         "title": "Venez nous écouter le 20 novembre 2021 à Vauvillers!",
-        "excerpt":"Le 20 novembre 2021, nous animons un repas donné à la salle des fêtes de Vauvillers, dans la cours du château, pour fêter l’arrivée du Beaujolais Nouveau. Vous pouvez participer à ce repas pour la somme de 20€ en réservant votre place auprès de Jean-François au 03.84.92.95.99 Au menu: Apéritif...","categories": ["concert"],
+        "excerpt":"Le 20 novembre 2021, nous animons un repas donné à la salle des fêtes de Vauvillers, dans la cour du château, pour fêter l’arrivée du Beaujolais Nouveau. Vous pouvez participer à ce repas pour la somme de 20€ en réservant votre place auprès de Jean-François au 03.84.92.95.99 Au menu: Apéritif...","categories": ["concert"],
         "tags": [],
         "url": "https://www.irishfever.fr//concert/20-novembre-vauvillers/"
       },{
         "title": "Venez nous écouter le 5 décembre 2021 à Pusey",
-        "excerpt":"Infos pratiques à venir ! ","categories": ["concert"],
+        "excerpt":"Concert à Pusey le 05 décembre de 14h à 15h, Maison Municipale et des Associations (6 bis rue du Breuil) dans le cadre du marché de Noël de Pusey. Entrée gratuite ;) ","categories": ["concert"],
         "tags": [],
         "url": "https://www.irishfever.fr//concert/5-d%C3%A9cembre-pusey-copy/"
       },{
         "title": "Venez nous écouter le 19 décembre 2021 à Luxeuil-les-Bains",
-        "excerpt":"Infos pratiques à venir ! ","categories": ["concert"],
+        "excerpt":"Concert à Luxeuil-les-Bains dimanche 19 décembre de 18h à 20h dans le cadre du marché de Noël, sur la place de l’Abbaye, en extérieur. Ce concert est gratuit ;) ","categories": ["concert"],
         "tags": [],
         "url": "https://www.irishfever.fr//concert/19-d%C3%A9cembre-luxeuil/"
       }]
